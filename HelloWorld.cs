f438d238a0bb7c6838342314c7d926db42d6f795
JustAnAlexBro <Zimberbusket@gmail.com>
2025-06-16 19:54:16 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // эта команда чтобы выводить инфу в консоль
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
